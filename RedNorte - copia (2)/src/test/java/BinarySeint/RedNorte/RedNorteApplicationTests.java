0ebcb88725a8fc5b80119326b3ce91b094bf7748
DoomedPlayer <ni.beeger@duocuc.cl>
2026-06-02 10:54:39 -0400

    Creación MonoRepo Red Norte Backend

---
+++ RedNorte - copia (2)/src/test/java/BinarySeint/RedNorte/RedNorteApplicationTests.java
@@ -0,0 +1,13 @@
+package BinarySeint.RedNorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedNorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ RedNorte - copia/src/main/java/BinarySeint/RedNorte/RedNorteApplication.java
@@ -0,0 +1,13 @@
+package BinarySeint.RedNorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedNorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedNorteApplication.class, args);
+	}
+
+}
