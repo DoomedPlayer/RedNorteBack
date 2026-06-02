964d8f039a7319b199e410a3111baf4b02671a90
bpalominosg <ben.palominos@duocuc.cl>
2026-06-02 16:56:11 -0400

    Raname Carpeta 2

--- auth-security-service/src/main/java/BinarySeint/RedNorte/AuthSecurityApplication.java
+++ auth-security-service/src/main/java/BinarySeint/RedNorte/AuthSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedNorteApplication {
+public class AuthSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedNorteApplication.class, args);
+		SpringApplication.run(AuthSecurityApplication.class, args);
 	}
 
 }
