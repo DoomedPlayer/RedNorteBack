1c8caeef45cb6e29ba22a521132eb22b95ff6e76
bpalominosg <ben.palominos@duocuc.cl>
2026-06-04 17:21:02 -0400

    Coneccion front-back-BD

---
+++ Patient-Portal/src/main/java/com/rednorte/portal/entities/Persona.java
@@ -0,0 +1,41 @@
+package com.rednorte.portal.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "persona")
+public class Persona {
+
+    @Id
+    @Column(name = "rut", length = 20)
+    private String rut;
+
+    @Column(name = "primer_nombre", nullable = false, length = 50)
+    private String primerNombre;
+
+    @Column(name = "segundo_nombre", length = 50)
+    private String segundoNombre;
+
+    @Column(name = "apellido_paterno", nullable = false, length = 50)
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno", nullable = false, length = 50)
+    private String apellidoMaterno;
+
+    @Column(name = "telefono", length = 20)
+    private String telefono;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "id_direccion")
+    private Integer idDireccion;
+}
